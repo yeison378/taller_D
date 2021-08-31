2760eb69d1ffb0d54f1e6cc5889a560f552aa5c8
yeison378 <yeisonalexis1@outlook.es>
2021-08-31 15:13:31 -0500

    pruebas junit

--- src/test/OccasionalTest.java
+++ src/test/OccasionalTest.java
@@ -1,20 +1,55 @@
 package test;
 
+import exeptions.ExeptionDate;
+import model.*;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class OccasionalTest {
 
-    @Test
-    void getTicketCost() {
+    Occasional h, q;
+    LocalDate antes, menor, mayor, despues;
+    Fly a, b;
+
+    public void prueba() throws ExeptionDate {
+        antes = LocalDate.of(2000, 1, 11);
+        mayor = LocalDate.of(2000, 1, 11);
+        menor = LocalDate.of(2002, 1, 11);
+        despues = LocalDate.of(2022, 2, 5);
+
+        a = new Fly("1", new Target("2", "nombre", 40, 455), new Airplane("1",
+                (short) 9, (short) 9, ETypeAirplane.AirbusA380), despues, LocalTime.of(5, 6));
+        b = new Fly("1", new Target("2", "nombre", 40, 455), new Airplane("1",
+                (short) 9, (short) 10, ETypeAirplane.AirbusA380), despues, LocalTime.of(5, 6));
+
+
+         h = new Occasional("3", "jess", "iban", "sogamo", menor, LocalDate.now());
+         q = new Occasional("9", "andre", "ibadn", "sogasdmo", menor, LocalDate.now());
+
+        a.addPassenger(h, (short) 4);
+        b.addPassenger(h, (short) 4);
+        h.addToFly(a, (short) 3);
+        h.addToFly(b, (short) 3);
+        h.getTicketCost();
+
+
     }
 
     @Test
-    void getDateLastFly() {
+    void getTicketCost() throws ExeptionDate {
+        prueba();
+        assertEquals(h.getTicketCost(), 40);
     }
 
     @Test
-    void setDateLastFly() {
+    void getDateLastFly() throws ExeptionDate {
+        prueba();
+        assertEquals(h.getDateLastFly(), LocalDate.now());
     }
+
+
 }
--- src/test/RegisteredTest.java
+++ src/test/RegisteredTest.java
@@ -1,36 +1,67 @@
 package test;
 
+import exeptions.ExeptionDate;
+import model.*;
 import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class RegisteredTest {
+    Registered k, p;
+    LocalDate antes, menor, mayor, despues;
+    Fly a, b;
 
-    @Test
-    void getTicketCost() {
+    public void prueba() throws ExeptionDate {
+        antes = LocalDate.of(2000, 1, 11);
+        mayor = LocalDate.of(2000, 1, 11);
+        menor = LocalDate.of(2002, 1, 11);
+        despues = LocalDate.of(2022, 2, 5);
+
+        a = new Fly("1", new Target("2", "nombre", 40, 455), new Airplane("1",
+                (short) 9, (short) 9, ETypeAirplane.AirbusA380), despues, LocalTime.of(5, 6));
+        b = new Fly("1", new Target("2", "nombre", 40, 455), new Airplane("1",
+                (short) 9, (short) 10, ETypeAirplane.AirbusA380), despues, LocalTime.of(5, 6));
+
+        k = new Registered("2", "yeison", "ibanez", "sogamoso", mayor, LocalDate.now());
+        p = new Registered("6", "karen", "zea", "ibague", menor, LocalDate.now());
+
+
+
+        a.addPassenger(k, (short) 4);
+        b.addPassenger(k, (short) 4);
+        k.addToFly(a, (short) 3);
+        k.addToFly(b, (short) 3);
+        k.getTicketCost();
+
     }
 
     @Test
-    void getMiles() {
+    void getTicketCost() throws ExeptionDate {
+        prueba();
+        assertEquals(k.getTicketCost(), 34);
     }
 
     @Test
-    void setMiles() {
+    void getMiles() throws ExeptionDate {
+        prueba();
+        assertEquals(k.getMiles(), 910);
+    }
+
+
+    @Test
+    void getDiscount() throws ExeptionDate {
+        prueba();
+        assertEquals(Registered.getDiscount(), 15);
     }
 
     @Test
-    void getDiscount() {
+    void getDateRegister() throws ExeptionDate {
+        prueba();
+        assertEquals(k.getDateRegister(), LocalDate.now());
     }
 
-    @Test
-    void setDiscount() {
-    }
 
-    @Test
-    void getDateRegister() {
-    }
-
-    @Test
-    void setDateRegister() {
-    }
 }
